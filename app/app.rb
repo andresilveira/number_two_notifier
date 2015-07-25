b01ce27de0fd99556dc5395dd512cd3078a39627
andresilveira <andresilveirah@gmail.com>
2015-07-25 18:40:24 +0200

    add app response

--- app/app.rb
+++ app/app.rb
@@ -1,2 +1,5 @@
 class App
+  def self.call(env)
+    [ENV['STATUS'].to_i, { "Content-Type" => "text/html" }, ["content"]]
+  end
 end
--- specs/app/app_spec.rb
+++ specs/app/app_spec.rb
@@ -2,7 +2,19 @@ require 'minitest/autorun'
 require_relative './../../app/app'
 
 describe 'App' do
-  it 'should not be nil' do
-    App.new.wont_be_nil
+  before do
+    @env = [200, { "Content-Type" => "text/html" }, ["content"]]
   end
+
+  describe '.call' do
+    it 'should return an array with 3 elements' do
+      App.call(@env).size.must_equal 3
+    end
+
+    it 'should contain the status from a env variable' do
+      ENV['STATUS'] = "400"
+      App.call(@env).first.must_equal 400
+    end
+  end
+
 end
